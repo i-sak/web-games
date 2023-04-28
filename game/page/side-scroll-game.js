5f8cf53d6b6d3502b402841232df551b77d66281
isaac <isaac7263@naver.com>
2023-04-29 01:51:47 +0900

    add: start 버튼 클릭 시 캐릭터가 나타남

--- game/page/side-scroll-game.js
+++ game/page/side-scroll-game.js
@@ -1,3 +1,7 @@
+/**
+ * Canvas
+ */
+
 // canvas
 const canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
@@ -10,11 +14,47 @@ canvas.height = 400; //window.innerHeight - 100;
 context.fillStyle = "#0099FF";
 context.fillRect(0,0,canvas.width, canvas.height);
 
+/**
+ * Character
+ */
+let image1 = new Image();
+image1.src = "hero-bazooka.png";
+
 
 // button
 const startButton = document.getElementById("startButton");
 startButton.addEventListener("click", function() {
     startButton.style.display = "none";
     console.log("게임 시작!");
-    // TODO: 게임 시작 로직 추가
+
+    togglePlayStatus();
+    startGame();
+
 });
+
+/**
+ * Status
+ */
+let play = false;
+
+const togglePlayStatus = () => {
+    play = !play;
+    console.log("play : " + play)
+}
+
+/**
+ * Start game
+ */
+const startGame = () => {
+    if (play) {
+        drawImage1();
+    }
+}
+
+/**
+ * Draw
+ */
+const drawImage1 = () => {
+    context.drawImage(image1, 10, 175, 50, 50);
+}
+
