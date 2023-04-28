282a9fee967ab90f236a6b5c1a5e8c1657bfec54
isaac <isaac7263@naver.com>
2023-04-29 01:01:33 +0900

    fix: update side-scroll-game

--- game/page/side-scroll-game.js
+++ game/page/side-scroll-game.js
@@ -3,10 +3,18 @@ const canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
 // size
-canvas.width = 1200; // window.innerWidth - 100;
-canvas.height = 500; //window.innerHeight - 100;
+canvas.width = 800; // window.innerWidth - 100;
+canvas.height = 400; //window.innerHeight - 100;
 
 // style
-context.fillStyle = "#113355";
+context.fillStyle = "#0099FF";
 context.fillRect(0,0,canvas.width, canvas.height);
 
+
+// button
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", function() {
+    startButton.style.display = "none";
+    console.log("게임 시작!");
+    // TODO: 게임 시작 로직 추가
+});
